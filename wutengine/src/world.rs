935928d878f68fd1957d878f800220692fce9b95
Wouter de Bruijn <wouter@wutru.io>
2024-07-22 21:53:22 +0200

    Adds support for mutable and optional components in queries. Does not work that great, though

--- wutengine/src/world.rs
+++ wutengine/src/world.rs
@@ -10,7 +10,6 @@ use wutengine_core::{
     component::{self, Component, ComponentTypeId},
     entity::EntityId,
 };
-use wutengine_util_macro::generate_component_filter_for_tuple;
 
 use crate::component::storage::ComponentStorage;
 
@@ -23,113 +22,6 @@ pub trait ComponentFilter<'a>: Sized {
         func: F,
     ) where
         F: for<'x> FnOnce(Vec<(EntityId, Self::Output<'x>)>);
-}
-
-// TODO: All these impls in a macro
-// TODO: Derive macro for using custom structs as a component filter
-
-impl<'a, A, B> ComponentFilter<'a> for (&'a A, &'a B)
-where
-    A: Component,
-    B: Component,
-{
-    type Output<'o> = (&'o A, &'o B);
-
-    fn filter<F>(
-        entities: &[EntityId],
-        components: &'a IntMap<ComponentTypeId, RefCell<ComponentStorage>>,
-        func: F,
-    ) where
-        F: for<'x> FnOnce(Vec<(EntityId, Self::Output<'x>)>),
-    {
-        let a_storage = components.get(&A::COMPONENT_ID).unwrap();
-        let b_storage = components.get(&B::COMPONENT_ID).unwrap();
-
-        let a = a_storage.borrow();
-        let b = b_storage.borrow();
-
-        let a_components = unsafe { a.get_multi::<A>(entities) };
-        let b_components = unsafe { b.get_multi::<B>(entities) };
-
-        let result = entities
-            .iter()
-            .copied()
-            .zip(itertools::izip!(a_components, b_components))
-            .filter(|(_, components)| components.0.is_some())
-            .filter(|(_, components)| components.1.is_some())
-            .map(|(id, components)| (id, (components.0.unwrap(), components.1.unwrap())))
-            .collect();
-
-        func(result);
-    }
-}
-
-impl<'a, A, B> ComponentFilter<'a> for (&'a mut A, &'a B)
-where
-    A: Component,
-    B: Component,
-{
-    type Output<'o> = (&'o mut A, &'o B);
-
-    fn filter<F>(
-        entities: &[EntityId],
-        components: &'a IntMap<ComponentTypeId, RefCell<ComponentStorage>>,
-        func: F,
-    ) where
-        F: for<'x> FnOnce(Vec<(EntityId, Self::Output<'x>)>),
-    {
-        let a_storage = components.get(&A::COMPONENT_ID).unwrap();
-        let b_storage = components.get(&B::COMPONENT_ID).unwrap();
-
-        let mut a = a_storage.borrow_mut();
-        let b = b_storage.borrow();
-
-        let a_components = unsafe { a.get_mut_multi::<A>(entities) };
-        let b_components = unsafe { b.get_multi::<B>(entities) };
-
-        let result = entities
-            .iter()
-            .copied()
-            .zip(itertools::izip!(a_components, b_components))
-            .filter(|(_, components)| components.0.is_some())
-            .filter(|(_, components)| components.1.is_some())
-            .map(|(id, components)| (id, (components.0.unwrap(), components.1.unwrap())))
-            .collect();
-
-        func(result);
-    }
-}
-
-impl<'a, A, B> ComponentFilter<'a> for (Option<&'a A>, &'a B)
-where
-    A: Component,
-    B: Component,
-{
-    type Output<'o> = (Option<&'o A>, &'o B);
-
-    fn filter<F>(
-        entities: &[EntityId],
-        components: &'a IntMap<ComponentTypeId, RefCell<ComponentStorage>>,
-        func: F,
-    ) where
-        F: for<'x> FnOnce(Vec<(EntityId, Self::Output<'x>)>),
-    {
-        let a = components.get(&A::COMPONENT_ID).unwrap().borrow();
-        let b = components.get(&B::COMPONENT_ID).unwrap().borrow();
-
-        let a_components = unsafe { a.get_multi::<A>(entities) };
-        let b_components = unsafe { b.get_multi::<B>(entities) };
-
-        let result = entities
-            .iter()
-            .copied()
-            .zip(itertools::izip!(a_components, b_components))
-            .filter(|(_, components)| components.1.is_some())
-            .map(|(id, components)| (id, (components.0, components.1.unwrap())))
-            .collect();
-
-        func(result);
-    }
 }
 
 pub struct World<'a> {
@@ -203,3 +95,5 @@ impl<'a> World<'a> {
         F::filter(self.entities, self.components, func);
     }
 }
+
+wutengine_util_macro::make_componentfilter_tuples!(5);
