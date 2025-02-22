5c3dc1ad68c906508f6b40263007480ed60e1ade
Wouter de Bruijn <wouter@wutru.io>
2025-02-22 12:18:09 +0100

    Preparation for enemy AI

--- wutengine_examples/pong/src/enemy.rs
+++ wutengine_examples/pong/src/enemy.rs
@@ -1,9 +1,25 @@
-use wutengine::component::Component;
+use wutengine::component::{Component, Context};
 use wutengine::macros::component_boilerplate;
 
 #[derive(Debug)]
-pub struct Enemy;
+pub struct Enemy {
+    pub max_speed: f32,
+    pub acceleration: f32,
+    cur_speed: f32,
+}
+
+impl Enemy {
+    pub fn new(max_speed: f32, acceleration: f32) -> Self {
+        Self {
+            max_speed,
+            acceleration,
+            cur_speed: 0.0,
+        }
+    }
+}
 
 impl Component for Enemy {
     component_boilerplate!();
+
+    fn update(&mut self, context: &mut Context) {}
 }
