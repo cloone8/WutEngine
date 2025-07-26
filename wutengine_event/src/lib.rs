c0a08023809f6d7f2b68b75d08caf39a3cfa7f83
Wouter de Bruijn <wouter@wutru.io>
2025-07-26 14:53:38 +0200

    Documented event library

--- wutengine_event/src/lib.rs
+++ wutengine_event/src/lib.rs
@@ -1,31 +1,53 @@
 //! WutEngine event handling and dispatching
+//!
+//! Provides a global crate where any other WutEngine sub-crate (and by extension, WutEngine users) can hook into and publish new events.
 
-use core::any::{Any, TypeId};
-use core::f32::consts::E;
+use core::any::TypeId;
 use core::fmt::Debug;
-use core::sync::atomic::AtomicU64;
 use std::collections::HashMap;
 use std::sync::mpsc::{Receiver, Sender};
 use std::sync::{Arc, Mutex};
 
+use wutengine_util::GlobalManager;
 use wutengine_util::hash::nohash_hasher::{IntMap, IntSet};
-use wutengine_util::{GlobalManager, TypeName};
 
-wutengine_util::unique_id_type!(EventSubscriptionId);
+mod api;
+mod subscription;
 
+pub use api::*;
+pub use subscription::*;
+
+/// The global [EventManager]
+static EVENT_MANAGER: GlobalManager<EventManager> = GlobalManager::new();
+
+/// The main event manager. Publishes events to a global channel, and then receives them
+/// and distributes them to subscribers once polled with [handle_pending_events].
 #[derive(Debug)]
-pub struct EventManager {
+struct EventManager {
+    /// Receiver channel. Only read through [handle_pending_events]
     event_recv: Mutex<Receiver<Box<dyn WutEngineEvent>>>,
+
+    /// Event sender channel
     event_send: Sender<Box<dyn WutEngineEvent>>,
+
+    /// Subscriber info
     subscribers: Mutex<Subscribers>,
 }
 
+/// A dynamic (untyped) event callback. Type checking and casting is probably done within the callback
+type DynamicEventCallback = dyn Fn(&dyn WutEngineEvent) + Send + Sync;
+
+/// All event subscribers known to a single [EventManager]
 struct Subscribers {
+    /// A mapping between event [TypeId] and subscribers, to make handling events more performant
     event_type_subscribers: HashMap<TypeId, IntSet<EventSubscriptionId>>,
-    subscribers: IntMap<EventSubscriptionId, Arc<dyn Fn(&dyn WutEngineEvent) + Send + Sync>>,
+
+    /// Contains the callbacks for each actual subscription
+    subscribers: IntMap<EventSubscriptionId, Arc<DynamicEventCallback>>,
 }
 
 impl Subscribers {
+    /// A new, empty, [Subscribers] map
     fn new() -> Self {
         Self {
             event_type_subscribers: HashMap::default(),
@@ -41,6 +63,7 @@ impl Debug for Subscribers {
 }
 
 impl EventManager {
+    /// A new, empty, [EventManager]
     fn new() -> Self {
         let (send, recv) = std::sync::mpsc::channel::<Box<dyn WutEngineEvent>>();
         Self {
@@ -48,129 +71,5 @@ impl EventManager {
             event_send: send,
             subscribers: Mutex::new(Subscribers::new()),
         }
-    }
-}
-
-static EVENT_MANAGER: GlobalManager<EventManager> = GlobalManager::new();
-
-pub fn init() {
-    GlobalManager::init(&EVENT_MANAGER, EventManager::new());
-}
-
-pub trait WutEngineEvent: Any + TypeName + Send + Sync {}
-
-#[profiling::function]
-pub fn publish<T: WutEngineEvent>(event: T) {
-    log::debug!("Publishing event of type {}", event.type_name());
-
-    EVENT_MANAGER
-        .event_send
-        .send(Box::new(event))
-        .expect("Failed to publish WutEngine event");
-}
-
-#[profiling::function]
-pub fn handle_pending_events() -> bool {
-    let event_recv = EVENT_MANAGER.event_recv.lock().unwrap();
-
-    let mut any_event = false;
-
-    let mut to_call = Vec::with_capacity(8);
-
-    for event in event_recv.try_iter() {
-        log::info!("Handling event of type {}", event.as_ref().type_name());
-
-        let event_type_id = (event.as_ref() as &dyn Any).type_id();
-
-        let subscribers = EVENT_MANAGER.subscribers.lock().unwrap();
-
-        let Some(type_subscriber_ids) = subscribers.event_type_subscribers.get(&event_type_id)
-        else {
-            continue;
-        };
-
-        for subscriber_id in type_subscriber_ids {
-            let subscriber_callback = subscribers
-                .subscribers
-                .get(subscriber_id)
-                .expect("Missing event subscriber");
-
-            to_call.push(Arc::clone(subscriber_callback));
-        }
-
-        drop(subscribers); // Drop subscribers so that new subscriptions do not deadlock
-
-        for callback in to_call.drain(..) {
-            callback(event.as_ref());
-        }
-
-        any_event = true;
-    }
-
-    any_event
-}
-
-#[profiling::function]
-pub fn subscribe<T: WutEngineEvent>(
-    callback: impl Fn(&T) + Send + Sync + 'static,
-) -> EventSubscription {
-    let subscription_id = EventSubscriptionId::new();
-
-    let mut subscribers = EVENT_MANAGER.subscribers.lock().unwrap();
-
-    subscribers
-        .event_type_subscribers
-        .entry(core::any::TypeId::of::<T>())
-        .or_default()
-        .insert(subscription_id);
-
-    subscribers.subscribers.insert(
-        subscription_id,
-        Arc::new(move |untyped_event| {
-            log::debug!(
-                "Notifying subscriber {subscription_id} of event of type {}",
-                core::any::type_name::<T>()
-            );
-
-            let typed_event = (untyped_event as &dyn Any)
-                .downcast_ref::<T>()
-                .expect("Invalid event type cast. Engine error");
-
-            callback(typed_event);
-        }),
-    );
-
-    EventSubscription(Arc::new(EventSubscriptionInner {
-        event_type: core::any::TypeId::of::<T>(),
-        subscriber_id: subscription_id,
-    }))
-}
-
-#[derive(Debug, Clone)]
-#[must_use = "Subscriptions only last while their IDs exist"]
-#[repr(transparent)]
-pub struct EventSubscription(Arc<EventSubscriptionInner>);
-
-#[derive(Debug)]
-struct EventSubscriptionInner {
-    event_type: TypeId,
-    subscriber_id: EventSubscriptionId,
-}
-
-impl Drop for EventSubscriptionInner {
-    #[profiling::function]
-    fn drop(&mut self) {
-        log::debug!("Unsubscribing subscription {}", self.subscriber_id);
-
-        let mut subscribers = EVENT_MANAGER.subscribers.lock().unwrap();
-
-        let Some(event_type_subscribers) =
-            subscribers.event_type_subscribers.get_mut(&self.event_type)
-        else {
-            return;
-        };
-
-        event_type_subscribers.remove(&self.subscriber_id);
-        subscribers.subscribers.remove(&self.subscriber_id);
     }
 }
---
+++ wutengine_event/src/subscription.rs
@@ -0,0 +1,46 @@
+//! Event subscriptions. Re-exported through the main lib file
+
+use core::any::TypeId;
+use std::sync::Arc;
+
+use crate::EVENT_MANAGER;
+
+wutengine_util::unique_id_type!(
+    /// The ID of a subscription to an event type
+    EventSubscriptionId
+);
+
+/// A subscription to a [WutEngineEvent](crate::WutEngineEvent). Internally ref counted, so cheap to clone. Once the last reference
+/// to the subscription is dropped, automatically unsubscribes from the event
+#[derive(Debug, Clone)]
+#[must_use = "Subscriptions only last while their IDs exist"]
+#[repr(transparent)]
+pub struct EventSubscription(pub(crate) Arc<EventSubscriptionInner>);
+
+/// The inner [EventSubscription]. Unsubscribes when dropped
+#[derive(Debug)]
+pub(crate) struct EventSubscriptionInner {
+    /// The type of the event
+    pub(crate) event_type: TypeId,
+
+    /// The subscription ID
+    pub(crate) subscriber_id: EventSubscriptionId,
+}
+
+impl Drop for EventSubscriptionInner {
+    #[profiling::function]
+    fn drop(&mut self) {
+        log::debug!("Unsubscribing subscription {}", self.subscriber_id);
+
+        let mut subscribers = EVENT_MANAGER.subscribers.lock().unwrap();
+
+        let Some(event_type_subscribers) =
+            subscribers.event_type_subscribers.get_mut(&self.event_type)
+        else {
+            return;
+        };
+
+        event_type_subscribers.remove(&self.subscriber_id);
+        subscribers.subscribers.remove(&self.subscriber_id);
+    }
+}
