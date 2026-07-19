984deb7d0ff9c828ddf58e65ac20ae6795d36baf
Wouter de Bruijn <wouter@wutru.io>
2026-07-19 15:59:04 +0200

    Utility function for combining UUIDs in const

--- runtime/wutengine/src/lib.rs
+++ runtime/wutengine/src/lib.rs
@@ -62,3 +62,34 @@ pub use wutengine_event as event;
 
 #[doc(inline)]
 pub use wutengine_task as task;
+
+pub mod util {
+    //! Utility functions
+
+    /// Combine two UUIDs into one. Usable in const contexts
+    pub const fn combine_uuid(a: uuid::NonNilUuid, b: uuid::NonNilUuid) -> uuid::NonNilUuid {
+        let bytes_a = a.get().into_bytes();
+        let bytes_b = b.get().into_bytes();
+
+        let bytes = [
+            bytes_a[0].wrapping_mul(bytes_b[0]),
+            bytes_a[1].wrapping_mul(bytes_b[1]),
+            bytes_a[2].wrapping_mul(bytes_b[2]),
+            bytes_a[3].wrapping_mul(bytes_b[3]),
+            bytes_a[4].wrapping_mul(bytes_b[4]),
+            bytes_a[5].wrapping_mul(bytes_b[5]),
+            bytes_a[6].wrapping_mul(bytes_b[6]),
+            bytes_a[7].wrapping_mul(bytes_b[7]),
+            bytes_a[8].wrapping_mul(bytes_b[8]),
+            bytes_a[9].wrapping_mul(bytes_b[9]),
+            bytes_a[10].wrapping_mul(bytes_b[10]),
+            bytes_a[11].wrapping_mul(bytes_b[11]),
+            bytes_a[12].wrapping_mul(bytes_b[12]),
+            bytes_a[13].wrapping_mul(bytes_b[13]),
+            bytes_a[14].wrapping_mul(bytes_b[14]),
+            bytes_a[15].wrapping_mul(bytes_b[15]),
+        ];
+
+        uuid::NonNilUuid::new(uuid::Builder::from_custom_bytes(bytes).into_uuid()).unwrap()
+    }
+}
